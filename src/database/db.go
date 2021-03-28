95cf983a1ee6cd38056f754f783ff849b9e6a4c5
Laio Andrade <laioand98@gmail.com>
2021-03-28 11:23:39 -0300

    change println to printf

--- src/database/db.go
+++ src/database/db.go
@@ -26,7 +26,7 @@ func connectDB() *gorm.DB {
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
-		fmt.Println("Error connecting to database : error=%v", err)
+		fmt.Printf("Error connecting to database : error=%v", err)
 		return nil
 	}
 
